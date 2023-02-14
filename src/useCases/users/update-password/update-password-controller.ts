e1c91339b0e88d3194da8882326cc86e410b1eae
Gabriel Renno <gabrielmrenno@gmail.com>
2023-02-14 20:28:23 -0300

    [FIX] only isAdmin user can update password from another user

--- src/useCases/users/update-password/update-password-controller.ts
+++ src/useCases/users/update-password/update-password-controller.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from "express";
+import { UsersRepository } from "../../../repositories/implementations/users-repository";
 import { UpdatePasswordUseCase } from "./update-password-usecase";
+
+const usersRepository = new UsersRepository();
 
 export class UpdatePasswordController {
   constructor(private updatePasswordUseCase: UpdatePasswordUseCase) {}
 
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
+    const { id: authenticateUser } = request.user!;
+
+    if (authenticateUser !== id) {
+      try {
+        const user = await usersRepository.findById(authenticateUser);
+        if (!user?.isAdmin) {
+          return response.status(401).json({ error: "User is not authorized" });
+        }
+      } catch (error) {
+        return response.status(400).json({
+          message: error.message || "Unexpected error.",
+        });
+      }
+    }
+
     const { password } = request.body;
 
     try {
