b8a3ff6acca9f824d4cfe9a84a80e758c46d214a
Gabriel Renno <gabrielmrenno@gmail.com>
2023-02-14 20:23:51 -0300

    [ADD] only isAdmin user can get data from another user

--- src/useCases/users/list-user-by-id/list-user-by-id-controller.ts
+++ src/useCases/users/list-user-by-id/list-user-by-id-controller.ts
@@ -1,20 +1,37 @@
 import { Request, Response } from "express";
+import { UsersRepository } from "../../../repositories/implementations/users-repository";
 import { ListUserByIdUseCase } from "./list-user-by-id-usecase";
+
+const usersRepository = new UsersRepository();
 
 export class ListUserByIdController {
   constructor(private listUserByIdUseCase: ListUserByIdUseCase) {}
 
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
+    const { id: authenticateUser } = request.user!;
 
-    // try {
-    const user = await this.listUserByIdUseCase.execute(id);
+    if (authenticateUser !== id) {
+      try {
+        const user = await usersRepository.findById(authenticateUser);
+        if (!user?.isAdmin) {
+          return response.status(401).json({ error: "User is not authorized" });
+        }
+      } catch (error) {
+        return response.status(400).json({
+          message: error.message || "Unexpected error.",
+        });
+      }
+    }
 
-    return response.status(200).json(user);
-    // } catch (error) {
-    // return response.status(400).json({
-    //     message: error.message || 'Unexpected error.',
-    // });
-    // }
+    try {
+      const user = await this.listUserByIdUseCase.execute(id);
+
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(400).json({
+        message: error.message || "Unexpected error.",
+      });
+    }
   }
 }
