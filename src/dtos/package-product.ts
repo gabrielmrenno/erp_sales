609bd265c7f6fb65791c16178fc75d0d03308aa2
gabrielrennoprime <grenno@ext.grupo-artico.com>
2023-02-17 11:02:38 -0300

    [ADD]created product entity

---
+++ src/dtos/package-product.ts
@@ -0,0 +1,9 @@
+interface ICreatePackageProduct {
+  description: string;
+  batch: string;
+  idProduct: string;
+  idProductionLaunch: string;
+  unit: ["FD", "SC"];
+  price: number;
+  weight: number;
+}
