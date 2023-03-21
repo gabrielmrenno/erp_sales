91f5e4bf5e8ba791b849b984db93825c6c3db120
Gabriel Renno <gabrielmrenno@gmail.com>
2023-03-21 20:48:01 -0300

    [ADD] config to deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import authenticateController from "../useCases/account/authenticate";
+import { AuthenticateController } from "../useCases/account/authenticate/authenticate-controller";
 import { userRoutes } from "./users.routes";
 
 const router = Router();
 
+const authenticateController = new AuthenticateController();
+
 router.use("/users", userRoutes);
 
-router.get("/auth/login", (request, response) => {
-  return authenticateController().handle(request, response);
-});
+router.get("/auth/login", authenticateController.handle);
 
 export { router };
