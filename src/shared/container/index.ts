4e08e3b4f34bae0f86672acee57c5fb7fbbafc91
Gabriel Renno <gabrielmrenno@gmail.com>
2023-03-22 21:23:27 -0300

    fix: importing tsyring's container in server.ts

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,9 @@
 import { container } from "tsyringe";
+
 import { UsersRepository } from "../../repositories/implementations/users-repository";
-import { UsersRepositoryInMemory } from "../../repositories/in-memory/users-repository-inmemory";
 import { IUsersRepository } from "../../repositories/users-repository-interface";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
-  process.env.NODE_ENV !== "test" ? UsersRepository : UsersRepositoryInMemory
+  UsersRepository
 );
