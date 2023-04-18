6dbe2437bcc50a126329f01a0c974e7b306bd251
Gabriel Renno <gabrielmrenno@gmail.com>
2023-04-18 15:55:31 -0300

    add: list customer by id implementation

--- src/http/routes/customers.route.ts
+++ src/http/routes/customers.route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { isAuthenticated } from "../../middleware/isAuthenticated";
 import { createCustomer } from "../controllers/customers/create-controller";
 import { listCustomers } from "../controllers/customers/list-controller";
+import { listCustomersByCode } from "../controllers/customers/list-by-code-controller";
 
 export const customerRoutes = Router();
 
 customerRoutes.use(isAuthenticated);
 
 customerRoutes.post("/", createCustomer);
-customerRoutes.get("/", listCustomers);
+customerRoutes.get("/all", listCustomers);
+customerRoutes.get("/:code", listCustomersByCode);
