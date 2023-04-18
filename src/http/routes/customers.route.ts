babeb914f6b0eebdeec1726980cdd4683b8fd5e5
Gabriel Renno <gabrielmrenno@gmail.com>
2023-04-18 16:26:02 -0300

    add: update customer implementation

--- src/http/routes/customers.route.ts
+++ src/http/routes/customers.route.ts
@@ -3,11 +3,18 @@ import { isAuthenticated } from "../../middleware/isAuthenticated";
 import { createCustomer } from "../controllers/customers/create-controller";
 import { listCustomers } from "../controllers/customers/list-controller";
 import { listCustomersByCode } from "../controllers/customers/list-by-code-controller";
+import { updateCustomer } from "../controllers/customers/update-controller";
 
 export const customerRoutes = Router();
 
 customerRoutes.use(isAuthenticated);
 
+// Create
 customerRoutes.post("/", createCustomer);
+
+// Read
 customerRoutes.get("/all", listCustomers);
 customerRoutes.get("/:code", listCustomersByCode);
+
+// Update
+customerRoutes.put("/:code", updateCustomer);
